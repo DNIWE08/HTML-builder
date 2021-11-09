24fefc4599b0b841f2c19bec0d360936f13f65e7
DNIWE08 <olimp12345678@gmail.com>
2021-11-09 22:51:38 +0300

    fix: remove old dir(copy-dir task)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,12 +5,21 @@ const path = require('path');
 const targetDir = path.resolve(__dirname, 'files');
 const outputDir = path.join(__dirname, 'copyFiles');
 
-function copyDir(targetDir, outputDir){
+async function copyDir(targetDir, outputDir){
+  
+  const rmOutputDir = await fsPromises.readdir(outputDir);
+  
+  rmOutputDir.forEach(file => {
+    const pathToFile = path.join(path.join(__dirname, 'copyFiles'), file);
+    fsPromises.unlink(pathToFile);
+  });
 
   fs.readdir(targetDir, (err, files) => {
     if (err) {
       console.log(err);
     } else {
+
+
       fs.mkdir(outputDir, { recursive: true }, (err) => {
         if (err) {
           console.log('something wrong ...');
